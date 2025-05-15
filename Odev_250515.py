719c88321fc964142b6e6a5ceb3c356068137e6d
Talha Yazıcı <talha.yazici@mehaz.com.tr>
2025-05-15 03:10:06 +0300

    Create Odev_250515.py

---
+++ Odev_250515.py
@@ -0,0 +1,102 @@
+#Kullanıcı, çalıştırmak istediği programı seçer
+secilenOdev = input(
+"""Lütfen ödev numarası seçiniz:
+1.Hesap Makinesi
+2.Gauss Toplamı
+3.Çift Sayılar
+4.Metin Çevirme
+Seçiminiz: """
+    )
+if secilenOdev == "1" or secilenOdev == "1.":
+    # Görev 1: Kullanıcıdan iki sayı alıp, aşağıdaki işlemleri yaparak ekrana yazdıran bir Python programı yazın:
+    # Toplama (+)
+    # Çıkarma (-)
+    # Çarpma (* )
+    # Bölme (/)
+    # Mod alma (%)
+    # Üs alma (** )
+
+    #Yapılacak işlem seçilir
+    yapilacakIslem = input(
+"""Lütfen yapılacak işlemi seçiniz:
+1. Toplama
+2. Çıkarma
+3. Çarpma
+4. Bölme
+5. Mod alma
+6. Üs alma
+Seçiminiz: """
+    )
+    #Kullanıcıdan sayılar alınır
+    birinciSayi = float(input("Birinci sayıyı giriniz: "))
+    ikinciSayi = float(input("İkinci sayıyı giriniz: "))
+    #Kontrol yapılabilmesi için none değeri atanır
+    sonuc = None
+    #Seçilen işleme göre kullanıcıdan alınan değerler işlenir
+    if yapilacakIslem == "1" or yapilacakIslem == "1.":
+        sonuc = birinciSayi + ikinciSayi
+    elif yapilacakIslem == "2" or yapilacakIslem == "2.":
+        sonuc = birinciSayi - ikinciSayi
+    elif yapilacakIslem == "3" or yapilacakIslem == "3.":
+        sonuc = birinciSayi * ikinciSayi
+    elif yapilacakIslem == "4" or yapilacakIslem == "4.":
+        sonuc = birinciSayi / ikinciSayi
+    elif yapilacakIslem == "5" or yapilacakIslem == "5.":
+        sonuc = birinciSayi % ikinciSayi
+    elif yapilacakIslem == "6" or yapilacakIslem == "6.":
+        sonuc = birinciSayi ** ikinciSayi
+    else: 
+        print("Lütfen işlem numarası seçiniz.")
+    #Hata kontrolü yapılır, hata yok ise kullanıcıya sonuç gösterilir
+    if sonuc != None:
+        print(sonuc)
+    else:
+        print("Bir hata oluştu lütfen tekrar deneyiniz")
+elif secilenOdev == "2" or secilenOdev == "2.":    
+    #Görev 2: Kullanıcıdan bir sayı alarak 1’den o sayıya kadar olan sayıların toplamını hesaplayan bir Python programı yazın. (For veya While döngüsü kullanın.)
+    #Kullanıcıdan sayı alınır
+    alinanSayi = int(input("Lütfen pozitif bir tam sayı giriniz: "))
+    #Saymaya 1 den başlanır
+    x = 1
+    #Kontrol için toplam'a 0 atanır
+    toplam = 0
+    #1 - alinanSayi arası sayma yapmak için x sayıya eşit ya da büyük oluncaya dek döngü sürer, 1 den küçük bir sayı girilmesi halinde döngüye başlamaması için büyüktür kullanılmıştır
+    while x <= alinanSayi:
+        toplam = toplam + x
+        x = x + 1
+    #Kontrol yapılır
+    if toplam == 0:
+        print("Lütfen tekrar deneyiniz")
+    else:
+        print(toplam)
+elif secilenOdev == "3" or secilenOdev == "3.":   
+    #Görev 3: 1 ile 100 arasındaki çift sayıları ekrana yazdıran bir Python programı yazın. 
+    ciftSayilar = []
+    x = 1
+    #1 den başlayarak yüze kadar bütün sayıların 2 ye göre modlarını alır, 0 olanları (bunlar çifttir) listeye ekle ve kullanıcıya sunar 
+    while x<=100:
+        if (x%2)==0:
+            ciftSayilar.append(x)
+        x=x+1
+    print(ciftSayilar)
+elif secilenOdev == "4" or secilenOdev == "4.":  
+    #Görev 4: Kullanıcıdan alınan bir metni ters çeviren ve ekrana yazdıran bir Python programı yazın. (Döngü kullanarak yapın.)
+    #Kullanıcıdan metin alınır
+    alinanMetin = input("Bir metin giriniz: ")
+    tersMetin = ""
+    #Metin içindeki karaktleri soldan sağa eklemeye başlar, selam yazılmış olsun -> 1.Adımda s + [] -> 2.Adımda e + [s] -> 3.Adımda l + [es] şeklinde devam eder
+    for karakter in alinanMetin:
+        tersMetin = karakter + tersMetin
+    print("Ters çevrilmiş metin:", tersMetin)
+else:
+    #Numara dışında bir şey seçmesi halinde uyarı verir
+    print("Lütfen ödev numarası seçiniz")
+
+
+
+
+
+
+
+
+
